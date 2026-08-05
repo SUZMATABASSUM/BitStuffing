dedc500ff3e014d7d4a27561fd959219a683280d
SUZMATABASSUM <shaikuzmatabasum@gmail.com>
2026-08-05 10:30:28 +0530

    Add files via upload

---
+++ cc.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    char str[1000];
+    int count = 0;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    while (str[count] != '\0') {
+        if (str[count] != '\n') {  // Ignore newline added by fgets
+            count++;
+        } else {
+            break;
+        }
+    }
+
+    printf("Number of characters = %d\n", count);
+
+    return 0;
+}
